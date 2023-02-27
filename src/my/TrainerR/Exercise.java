cc073dada7e95e7975c750bb10f72fdc6e27694d
criiz <criiz@DESKTOP-8P4CSG3>
2023-02-26 20:06:55 -0600

    First Repository 0_0

---
+++ src/my/TrainerR/Exercise.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package my.TrainerR;
+
+/**
+ *
+ * @author criiz
+ */
+public class Exercise {
+    String name = "";
+    String type = "";
+    
+    public void setName(String newname){
+        name = newname;
+    }
+    public void setType(String newname){
+        type = newname;
+    }
+    
+}
---
+++ src/my/TrainerR/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package my.TrainerR;
+
+/**
+ *
+ * @author criiz
+ */
+public class Main {
+    int x = 69;
+    public void setX(int num){
+        x = num;
+    }
+}
